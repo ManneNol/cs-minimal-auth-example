626358ed0921209a0591d059dfaeeab8fd9d30f6
Manne <manne@mosslightstudio.com>
2024-03-28 15:01:56 +0100

    fleshed out the example code

--- Server/Auth.cs
+++ Server/Auth.cs
@@ -1,22 +1,41 @@
-﻿namespace Server;
+﻿using MySql.Data.MySqlClient;
+
+namespace Server;
 
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 
 public class Auth
 {
-    public static async Task<IResult> Login(HttpContext ctx)
-    {
-        // here you should query the database about the info sent from the client
-        // and return early if its incorrect
-        // return TypedResults.Problem("Wrong email or password")
+    public record LoginData(string Email, string Password);
 
+    public static async Task<IResult> Login(LoginData user, State state, HttpContext ctx)
+    {
+        MySqlCommand cmd = new("select id, role from users where email = @Email and password = @Password", state.db);
+        
+        cmd.Parameters.AddWithValue("@Email", user.Email);
+        cmd.Parameters.AddWithValue("@Password", user.Password);
+        
+        using var reader = cmd.ExecuteReader();
+        
+        // Check if any are found, if none it returns false, if 1 or more, it gives access to them
+        bool bIsFound = reader.Read();
+        
+        if (!bIsFound)
+        {
+            return TypedResults.Problem("No Such User Exists");
+        }
+        
+        string id = reader.GetInt32("id").ToString();
+        string role = reader.GetString("role");
+        
+        Console.WriteLine(id + ", " + role);
         await ctx.SignInAsync("opa23.teachers.foodcourt", new ClaimsPrincipal(
             new ClaimsIdentity(
                 new Claim[]
                 {
-                    new Claim(ClaimTypes.NameIdentifier, "my_name"),
-                    new Claim(ClaimTypes.Role, "admin"),
+                    new Claim(ClaimTypes.NameIdentifier, id),
+                    new Claim(ClaimTypes.Role, role),
                 },
                 "opa23.teachers.foodcourt"
             )
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,21 @@
+using MySql.Data.MySqlClient;
 using Server;
+
+State state = new State(new("server=localhost;uid=root;pwd=password;database=cs_demo;port=3307"));
+try
+{
+    state.db.Open();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication().AddCookie("opa23.teachers.foodcourt");
 builder.Services.AddAuthorizationBuilder().AddPolicy("admin_route", policy => policy.RequireRole("admin"));
-
+builder.Services.AddSingleton(state);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
@@ -11,3 +23,5 @@ app.MapPost("/login", Auth.Login);
 app.MapGet("/admin", () => "Hello, Admin!").RequireAuthorization("admin_route");
 
 app.Run("http://localhost:3001");
+
+public record State(MySqlConnection db);
