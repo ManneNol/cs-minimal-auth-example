defe64c162ab1a7f201a955c4312b787149232af
Manne <manne@mosslightstudio.com>
2024-03-28 10:58:17 +0100

    example from lecture

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,13 @@
+using Server;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication().AddCookie("opa23.teachers.foodcourt");
+builder.Services.AddAuthorizationBuilder().AddPolicy("admin_route", policy => policy.RequireRole("admin"));
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.Run();
+app.MapPost("/login", Auth.Login);
+app.MapGet("/admin", () => "Hello, Admin!").RequireAuthorization("admin_route");
+
+app.Run("http://localhost:3001");
